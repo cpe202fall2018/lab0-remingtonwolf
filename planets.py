6271c45427b6a876038036da12a65b1d0c31d466
remingtonwolf <rewolf@calpoly.edu>
2018-09-24 10:28:43 -0700

    Added functionality

--- planets.py
+++ planets.py
@@ -1,7 +1,13 @@
+MARS_WEIGHT_MULTIPLIER = 0.38
+JUPITER_WEIGHT_MULTIPLIER = 2.34 # used to calculate weights on other planets
+
 def weight_on_planets():
-   # write your code here
-   
-   
-   
+	weightOnEarth = float(input("What do you weigh on earth? ")) # get user input then convert to float before calculations
+	weightOnMars = weightOnEarth * MARS_WEIGHT_MULTIPLIER
+	weightOnJupiter = weightOnEarth * JUPITER_WEIGHT_MULTIPLIER # get weight on Jupiter and Mars
+	print("\n" +
+		"On Mars you would weigh", weightOnMars, "pounds.\n" +
+		"On Jupiter you would weigh", weightOnJupiter, "pounds.") # print results using \n for new line
+	
 if __name__ == '__main__':
    weight_on_planets()
